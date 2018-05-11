ffc9425bb0ef47c246f383bd376387628982867d
Tony Gonzalez <Tony@AntoniosMacBook.localdomain>
2018-05-10 21:50:11 -0700

    more fixes

--- server.js
+++ server.js
@@ -6,7 +6,7 @@
 // =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
-var db = require("./app/models/index.js");
+var db = require("./app/models");
 
 
 // Sets up the Express App
@@ -17,12 +17,19 @@ var PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true })); 
 // parse application/json
 app.use(bodyParser.json());
 
 // Static directory
 app.use(express.static("app/public"));
+
+
+app.get('/triggers', function (req, res) {
+  db.Triggers.findAll({}).then(function (triggers) {
+    console.log(triggers)
+  })
+})
 
 // Starts the server to begin listening
 // =============================================================
@@ -30,7 +37,9 @@ app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
 
-db.sequelize.sync({ force: true }).then(function () {
+
+
+db.sequelize.sync().then(function () {
   app.listen(PORT, function () {
     console.log("app listening on PORT " + PORT);
   });
