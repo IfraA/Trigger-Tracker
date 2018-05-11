bfe07e0564d782600aa826a203fe4e6c643b28f7
Tony Gonzalez <Tony@AntoniosMacBook.localdomain>
2018-05-10 21:13:15 -0700

    more sequelize adjustments

--- app/models/triggers.js
+++ app/models/triggers.js
@@ -1,12 +1,21 @@
 module.exports = function (sequelize, DataTypes) {
-  var Burgers = sequelize.define("triggers", {
-    triggers: {
+  var Triggers = sequelize.define("Triggers", {
+    triggerName: {
       type: DataTypes.STRING,
       allowNull: false,
+      default: 0,
       validate: {
         len: [1]
       }
     },
+    userName: {
+      type: DataTypes.STRING,
+      default: 0,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    }
   });
-  return triggers;
+  return Triggers;
 };
