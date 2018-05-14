0f7841798b986c1715399aa309df20348e4c9447
Maureen Bockrath <maureenbockrath@Maureens-MacBook-Air.local>
2018-05-14 01:09:52 -0700

    messed with the package.json

--- app/public/js/front.js
+++ app/public/js/front.js
@@ -18,14 +18,14 @@ $(document).ready(function () {
                         getCordsLocation(position.coords.latitude + "," + position.coords.longitude);
                         setTimeout(function () {
                             window.location.href = "/index";
-                        }, 3000);
+                        }, 1250);
                     },
                     function error(error_message) {
                         // for when getting location results in an error
                         console.error('An error has occured while retrieving location', error_message);
                         setTimeout(function () {
                             window.location.href = "/index";
-                        }, 3000);
+                        }, 1250);
                     }
                 );
             } else if (result.state === 'prompt') {
@@ -36,14 +36,14 @@ $(document).ready(function () {
                         getCordsLocation(position.coords.latitude + "," + position.coords.longitude);
                         setTimeout(function () {
                             window.location.href = "/index";
-                        }, 3000);
+                        }, 1250);
                     },
                     function error(error_message) {
                         // for when getting location results in an error
                         console.error('An error has occured while retrieving location', error_message);
                         setTimeout(function () {
                             window.location.href = "/index";
-                        }, 3000);
+                        }, 1250);
                     }
                 );
             }
@@ -57,48 +57,11 @@ $(document).ready(function () {
     }
 
 
-    // if ("geolocation" in navigator) {
-
-    //     navigator.permissions.query({ name: 'geolocation' }).then(function (result) {
-    //         console.log(result);
-    //         if (result.state === 'granted') {
-    //             console.log("Latitude: " + result.coords.latitude +
-    //                 "Longitude: " + result.coords.longitude);
-    //             getCordsLocation(result.coords.latitude + "," + result.coords.longitude);
-    //         } else if (result.state === 'prompt') {
-    //             // check if geolocation is supported/enabled on current browser
-    //             navigator.geolocation.getCurrentPosition(
-    //                 function success(position) {
-    //                     // for when getting location is a success
-    // console.log("Latitude: " + position.coords.latitude + "Longitude: " + position.coords.longitude);
-    // getCordsLocation(position.coords.latitude + "," + position.coords.longitude);
-
-    //                 },
-    //                 function error(error_message) {
-    //                     // for when getting location results in an error
-    //                     console.error('An error has occured while retrieving location', error_message);
-    // setTimeout(function () {
-    //     window.location.href = "/index";
-    // }, 3000);
-    //                 }
-    //             )
-    //         }
-    //     });
-    // } else {
-    //     // geolocation is not supported
-    //     // get your location some other way
-    //     console.log('geolocation is not enabled on this browser');
-    //     //     setTimeout(function () {
-    //     //         window.location.href = "/index";
-    //     //     }, 3000);
-    // }
-
-
     //function that uses cords to generate location key api
     function getCordsLocation(currentCords) {
         // var currentLat = position.coords.latitude;
         // var currentLong = position.coords.longitude;
-        var queryURL = "http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=qiFHdGlcXwcyPvEO6lVxQ5YlYpqfGCs8&q=" + currentCords + "&language=en-us&details=true";
+        var queryURL = "http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=CJR5xPcfo0AAeqd9dqsWy5XYd2FCKzSD&q=" + currentCords + "&language=en-us&details=true";
         $.ajax({
             url: queryURL,
             method: "GET",
@@ -110,17 +73,10 @@ $(document).ready(function () {
             dailyTemp();
             setTimeout(function () {
                 window.location.href = "/index";
-            }, 3000);
-
+            }, 1250);
         });
 
     }
-
-
-    // $("#submit").on("click", function () {
-    //     getCityLocation();
-
-    // });
 
     //if user blocks use location key to let them add their location
     function getCityLocation() {
@@ -128,7 +84,7 @@ $(document).ready(function () {
         // var locationKey = "";
         // var city = document.getElementById('#userInput').value;
         var city = $('#inputZip').val();
-        var queryURL = "http://dataservice.accuweather.com/locations/v1/cities/search?apikey=qiFHdGlcXwcyPvEO6lVxQ5YlYpqfGCs8&q=" + city + "&language=en-us&details=true&alias=Always";
+        var queryURL = "http://dataservice.accuweather.com/locations/v1/cities/search?apikey=CJR5xPcfo0AAeqd9dqsWy5XYd2FCKzSD&q=" + city + "&language=en-us&details=true&alias=Always";
         $.ajax({
             url: queryURL,
             method: "GET",
@@ -142,11 +98,9 @@ $(document).ready(function () {
 
     }
 
-
-
     // function get dailyforecast for temperature
     function dailyTemp() {
-        var queryURL = "http://dataservice.accuweather.com/forecasts/v1/daily/1day/" + locationKey + "?apikey=qiFHdGlcXwcyPvEO6lVxQ5YlYpqfGCs8&language=en-us&details=true&metric=false";
+        var queryURL = "http://dataservice.accuweather.com/forecasts/v1/daily/1day/" + locationKey + "?apikey=CJR5xPcfo0AAeqd9dqsWy5XYd2FCKzSD&language=en-us&details=true&metric=false";
         $.ajax({
             url: queryURL,
             method: "GET",
@@ -158,7 +112,7 @@ $(document).ready(function () {
             $("#weather").append(response.DailyForecasts[0].Temperature.Maximum.Value + " " + response.DailyForecasts[0].Temperature.Maximum.Unit);
             var iconName = response.DailyForecasts[0].Day[0].IconPhrase;
             if (iconName === "Sunny") {
-                ("#weather").append("<img src = assets/sunny.png>")
+                ("#weather").append("<img src = assets/sunny.png>");
             }
             $("#weather").append(response.DailyForecasts[0].AirAndPollen[0].Name + "<br>" + response.DailyForecasts[0].AirAndPollen[0].Value + "<br>" + response.DailyForecasts[0].AirAndPollen[0].Category + response.DailyForecasts[0].Day.Icon);
         });
