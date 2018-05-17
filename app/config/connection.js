237953e7f568be191fb1aa29dc14899092243936
Maureen Bockrath <maureenbockrath@Maureens-MacBook-Air.local>
2018-05-16 23:51:07 -0700

    connection fingers crossed

--- app/config/connection.js
+++ app/config/connection.js
@@ -1,13 +1,18 @@
 var Sequelize = require("sequelize");
-var sequelize = new Sequelize("triggers", "root", "root", {
-  host: "localhost",
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
-});
+
+if (process.env.JAWSDB_URL) {
+  var sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize("triggers", "root", "root", {
+    host: "localhost",
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000
+    }
+  });
+}
 
 // Exports the connection for other files to use
 module.exports = sequelize;
