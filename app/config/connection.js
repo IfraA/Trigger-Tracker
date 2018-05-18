2cb9dae1b5bafe7b2b8777769d0d169ed94ad1e7
Maureen Bockrath <maureenbockrath@Maureens-Air.localdomain>
2018-05-17 17:53:06 -0700

    conection fix?

--- app/config/connection.js
+++ app/config/connection.js
@@ -1,18 +1,14 @@
 var Sequelize = require("sequelize");
 
-if (process.env.JAWSDB_URL) {
-  var sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize("triggers", "root", "root", {
-    host: "localhost",
-    dialect: "mysql",
-    pool: {
-      max: 5,
-      min: 0,
-      idle: 10000
-    }
-  });
-}
+sequelize = new Sequelize("triggers", "root", "root", {
+  host: "localhost",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
+});
 
 // Exports the connection for other files to use
 module.exports = sequelize;
